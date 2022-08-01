8feb38b411f99092fa167d31710b72a670c75016
Him5696 <himanshi1252@gmail.com>
2022-08-01 13:42:47 +0530

    Database Connection

--- src/com/bridgelabz/database/MySqlDB.java
+++ src/com/bridgelabz/database/MySqlDB.java
@@ -1,7 +1,18 @@
 package com.bridgelabz.database;
 
+import java.sql.*;
+
 public class MySqlDB {
     public static void main(String[] args) {
-        System.out.println("Welcome to jdbc program");
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/payroll_servicedb", "root", "Himanshi@12345");
+            Statement statement = con.createStatement();
+            System.out.println("Connection done!");
+            statement.close();
+            con.close();
+        } catch (ClassNotFoundException | SQLException e){
+            System.out.println("exception" + e);
+        }
     }
 }
