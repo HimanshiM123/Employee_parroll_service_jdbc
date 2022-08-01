b11f98c73b64e3ee304602bdc5cb2893429d6c22
Him5696 <himanshi1252@gmail.com>
2022-08-01 14:27:04 +0530

    Retrieving payroll data by name

--- src/com/bridgelabz/database/MySqlDB.java
+++ src/com/bridgelabz/database/MySqlDB.java
@@ -4,21 +4,14 @@ import java.sql.*;
 
 public class MySqlDB {
     public static void main(String[] args) {
-        double salary = 600000.00;
-        int id = 4;
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/payroll_servicedb", "root", "Himanshi@12345");
             Statement statement = con.createStatement();
             System.out.println("Connection done!");
-            String query = "update employee_payroll set salary = ? WHERE id = ?;";
-            PreparedStatement preparedStatement = con.prepareStatement(query);
-            preparedStatement.setDouble(1, salary);
-            preparedStatement.setInt(2, id);
-            preparedStatement.executeUpdate();
-            
-            String query1 = "select * from employee_payroll;";
-            ResultSet rs = statement.executeQuery(query1);
+            PreparedStatement preparedStatement = con.prepareStatement("select * from employee_payroll where name =?;");
+            preparedStatement.setString(1, "Terissa");
+            ResultSet rs = preparedStatement.executeQuery();
             while (rs.next()) {
                 System.out.println(rs.getInt("id") + " | " + rs.getString("name") + " | " + rs.getString("gender") + " | " + rs.getString("phone_number") + " | "
                         + rs.getString("address") + " | " + rs.getString("department") + " | " + rs.getDouble("salary") + " | " + rs.getDouble("deduction") + " | "
