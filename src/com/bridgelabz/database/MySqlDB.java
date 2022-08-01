5e71a8cb2a3ebacc3d2a73d0ca1d5958e750595b
Him5696 <himanshi1252@gmail.com>
2022-08-01 13:54:09 +0530

    Retrieving data from employee_payroll

--- src/com/bridgelabz/database/MySqlDB.java
+++ src/com/bridgelabz/database/MySqlDB.java
@@ -4,15 +4,23 @@ import java.sql.*;
 
 public class MySqlDB {
     public static void main(String[] args) {
-        try{
+        try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/payroll_servicedb", "root", "Himanshi@12345");
             Statement statement = con.createStatement();
             System.out.println("Connection done!");
+            String query = "select * from employee_payroll;";
+            ResultSet rs = statement.executeQuery(query);
+            while (rs.next()) {
+                System.out.println(rs.getInt("id") + " | " + rs.getString("name") + " | " + rs.getString("gender") + " | " + rs.getString("phone_number") + " | "
+                        + rs.getString("address") + " | " + rs.getString("department") + " | " + rs.getDouble("salary") + " | " + rs.getDouble("deduction") + " | "
+                        + rs.getDouble("taxablePay") + " | " + rs.getDouble("netPay") + " | " + rs.getDouble("incomeTax") + " | " + rs.getDate("start"));
+            }
             statement.close();
             con.close();
-        } catch (ClassNotFoundException | SQLException e){
+        } catch (ClassNotFoundException | SQLException e) {
             System.out.println("exception" + e);
         }
     }
 }
+
